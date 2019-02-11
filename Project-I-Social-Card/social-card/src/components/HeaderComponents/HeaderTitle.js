811bd39a7d8d5f0ee07692d79e53322e17c1e16f
unknown <38929577+meginks@users.noreply.github.com>
2019-02-11 15:34:46 -0600

    got header elements to appear in browser

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    return (
+        <h1>Lambda School <em>@lambdaschool</em> <span>26 Jan</span> </h1>
+    );
+}; 
+
+export default HeaderTitle; 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return (
+        <div className="ImageThumbnail">
+            <img src="logo.png" alt="logo"></img>
+        </div>
+    );
+};
+
+
+
+export default ImageThumbnail; 
