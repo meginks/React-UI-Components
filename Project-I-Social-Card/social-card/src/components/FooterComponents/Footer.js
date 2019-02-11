c1335a90121efafb7d1672c461798223b74a54c6
unknown <38929577+meginks@users.noreply.github.com>
2019-02-11 17:16:45 -0600

    added font awesome icons for footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,7 +3,22 @@ import '../FooterComponents/Footer.css';
 
 const Footer = () => {
     return (
-        <footer>This is my footer.</footer>
+        <div className="footer">
+        <footer>
+            <div className="footer-icon">
+            <i className="far fa-comment"></i>  
+            </div>
+            <div className="footer-icon"><i className="fas fa-retweet"></i> 6
+            </div>
+            <div className="footer-icon">
+            <i className="far fa-heart"></i> 
+             4
+            </div>
+            <div className="footer-icon">
+            <i className="far fa-envelope"></i>
+            </div>
+        </footer>
+        </div>
     );
 };
 
