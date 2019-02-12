a3571c409269479633067b02660a07c9ac8bc993
unknown <38929577+meginks@users.noreply.github.com>
2019-02-12 15:19:55 -0600

    got rid of css files and added less, installed yarn in calculator folder, created operators and operator container

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/OperatorContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OperatorContainer = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+
+};
+
+export default OperatorContainer;
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Operators.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Divide = {
+    operation: "divide", 
+    symbol: "&#247"
+}
+
+const Multiply = {
+    operation: "multiply", 
+    symbol: "X"
+}
+
+const Subtract = {
+    operation: "subtract",
+    symbol: "-"
+}
+
+const Equals = {
+    operation: "equals",
+    symbol: "="
+}
