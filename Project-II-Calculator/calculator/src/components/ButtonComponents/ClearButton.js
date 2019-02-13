d4dd9401dd612b9d23e8114543ec7462e226c132
unknown <38929577+meginks@users.noreply.github.com>
2019-02-12 18:17:43 -0600

    fixed clear button

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-const Clear = {
-    name: "clear",
-    symbol: "clear"
-}; 
+
 
 const ClearButton = props => {
     return (
         <button className="clear-button">
-            {props.name}
+            clear
         </button>
     );
 };
